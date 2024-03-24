2a7c64415036570a7f98187fa95f11d9f0a93c53
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2024-03-24 16:35:39 -0500

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {Navigation} from './presentation/navigation/Navigation';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+};
---
+++ src/presentation/navigation/Navigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {DetailsScreen} from '../screens/details/DetailsScreen';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+
+export type RootStackParams = {
+  Home: undefined;
+  Details: {movieId: number};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
